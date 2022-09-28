9061f87146ba23b82a7a22944f9f9de82d18badb
Louis Milhaud <louismilhaud@Mac-mini-de-Louis.local>
2022-09-28 16:06:28 +0200

    add init.lua and gitignore

---
+++ init.lua
@@ -0,0 +1,22 @@
+--[[ init.lua ]]
+
+-- LEADER
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = ","
+vim.g.localleader = "\\"
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- IMPORTS
+require('vars')      -- Variables
+require('opts')      -- Options
+require('keys')      -- Keymaps
+require('plug')      -- Plugins
+
+-- PLUGINS
+require('nvim-tree').setup()
+require('lualine').setup()
+require('nvim-autopairs').setup()
+--require('nvim-web-devicons').setup()
+require('vgit').setup()
